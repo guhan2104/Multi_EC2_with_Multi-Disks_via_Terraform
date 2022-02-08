d27897a780cc8d6dfe2b1e883368106ef50eb58a
suresh kumar <suressubramanian2013@gmail.com>
2022-02-09 04:59:22 +0530

    provider-change

--- provider.tf
+++ provider.tf
@@ -1,13 +1,8 @@
 provider "aws" {
-  version = "~> 2.20.0"
+  version = ">= 3.20.0"
 
-  region = "us-east-1"
 }
 
-provider "aws" {
-  alias = "prod"
-  region = "us-east-2"
-}
 
 terraform {
   required_providers {
